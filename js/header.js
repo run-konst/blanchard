205f3c1dff2cdf94d417dc1f701097d9dc327ac0
run-konst <72573199+run-konst@users.noreply.github.com>
2021-01-11 14:36:29 +0300

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.alt-nav__btn');
+const dropdowns = document.querySelectorAll('.select-painter');
+
+
+let clickButton = function(button, dropdown) {
+    button.addEventListener('click', function() {
+        for(let i = 0; i < buttons.length; i++) {
+            if(buttons[i] !== button) {
+                buttons[i].classList.remove('alt-nav__btn_active');
+                dropdowns[i].classList.add('hidden');
+            }
+        }
+        button.classList.toggle('alt-nav__btn_active');
+        dropdown.classList.toggle('hidden');
+    })    
+}
+
+for(let i = 0; i < buttons.length; i++) {
+    clickButton(buttons[i], dropdowns[i]);
+}
+
